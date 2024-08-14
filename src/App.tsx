5cb6b4606a28693cbbf045efdbad8b201eb66065
Bernardo Marques <bernardohmarques@gmail.com>
2024-08-13 22:03:15 -0300

    initial structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import MyRoutes from "./Routes";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <MyRoutes />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
